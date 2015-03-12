69e092b4af67beabf4dff7cf173c96802daebe3c
Robert Isele <mail@robertisele.com>
2015-03-12 13:14:59 +0100

    Fixed news page

--- generator/app/Render.scala
+++ generator/app/Render.scala
@@ -16,7 +16,7 @@ object Render extends App {
 
   // Render html pages
   write("index", views.html.index.render())
-  write("news", views.html.index.render())
+  write("news", views.html.news.render())
   write("download", views.html.download.render())
   write("support", views.html.support.render())
   write("publications", views.html.publications.render())
